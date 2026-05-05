f98567ec5fc91911f28550475f5cb57e66d21105
woals <woals6318@gmail.com>
2026-05-05 16:33:55 +0900

    Revert "Create RepositoryModule to bind TimerRepository implementation"
    This reverts commit 68aaa03ec51b367d7c97196e730734a9c61d647a.

--- app/src/main/java/com/example/ca_prac/presentation/timer/TimerUiState.kt
+++
@@ -1,9 +0,0 @@
-package com.example.ca_prac.presentation.timer
-
-import com.example.ca_prac.domain.model.TimerType
-
-data class TimerUiState(
-    val formattedTime: String = "25:00",
-    val isRunning: Boolean = false,
-    val type: TimerType = TimerType.WORK
-)
