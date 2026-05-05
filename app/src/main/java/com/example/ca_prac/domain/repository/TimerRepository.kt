58cd79cb6a74ab1e2d80ccc4f1e14627883541dd
woals <woals6318@gmail.com>
2026-05-05 11:16:30 +0900

    Create TimerRepository interface

---
+++ app/src/main/java/com/example/ca_prac/domain/repository/TimerRepository.kt
@@ -0,0 +1,11 @@
+package com.example.ca_prac.domain.repository
+import com.example.ca_prac.domain.model.TimerEntity
+import com.example.ca_prac.domain.model.TimerType
+import kotlinx.coroutines.flow.Flow
+
+interface TimerRepository {
+    fun getTimerFlow(): Flow<TimerEntity>
+    suspend fun startTimer()
+    suspend fun pauseTimer()
+    suspend fun updateTimer(type: TimerType)
+}
